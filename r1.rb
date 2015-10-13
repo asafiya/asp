dbaa4b0c8b74195f3ea8df4cc238413e4a9ed759
asafiya <aspkareem@gmail.com>
2015-10-13 20:44:12 +0530

    created ruby &shll

---
+++ r1.rb
@@ -0,0 +1,10 @@
+a=gets.chomp
+aa=a.to_i()
+b=gets.chomp
+bb=b.to_i()
+c=gets.chomp
+cc=c.to_i()
+d=aa+bb+cc
+puts "Added value #{d}"
+e=aa-bb-cc
+puts "Subtracted value #{e}"
---
+++ r2.rb
@@ -0,0 +1,8 @@
+anum=gets.chomp
+anumi=anum.to_i()
+if anumi%2==0
+   puts "#{anumi} is even"
+else
+   puts "#{anumi} is odd"
+end
+
